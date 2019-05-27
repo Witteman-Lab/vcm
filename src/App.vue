b953129a74a58c4d94c0c0998ca80819417305e0
Martin T.-B <martin_t_b@hotmail.com>
2019-05-27 12:12:57 -0400

    Changes to build sliders dynamically

--- src/App.vue
+++ src/App.vue
@@ -13,129 +13,90 @@
             <span id="scaleLeft">{{this.labels["left scale label"]}}</span>
             <span id="scaleRight">{{this.labels["right scale label"]}}</span>
             <br>
-            <SliderRange></SliderRange>
-            <!--<div ref="slider" v-for="(SliderRange) in sliderRanger " :key="SliderRange.id">-->
-                <!--<SliderRange></SliderRange>-->
-            <!--</div>-->
+            <!-- <SliderRangeTest></SliderRangeTest> -->
+            <!-- <div ref="sliders" v-for="(slider) in this.numberOfSlider" :key="SliderRangeTest.id"> -->
+            <div v-for="(slider) in this.numberOfSlider">
+                <SliderRange></SliderRange>
+            </div>
             <span >{{this.labels["top slider’s label"]}}</span>
         </div>
-
-        <!--<div id="slider" class="column is-half is-center has-text-centered">-->
-           <!--<div class="column has-text-centered">-->
-               <!--<div id="slidersScale">-->
-                   <!--<span class="sliderScale editableText" id="scaleLeft"><strong>{{this.labels["left scale label"]}}</strong></span>-->
-                   <!--<span class="sliderScale editableText" id="scaleRight"><strong>{{this.labels["right scale label"]}}</strong></span>-->
-                   <!--<div >-->
-                       <!--<br>-->
-                       <!--<SliderRange> </SliderRange>-->
-                       <!--<span >{{this.labels["top slider’s label"]}}</span>-->
-                   <!--</div>-->
-               <!--</div>-->
-           <!--</div>-->
-            <!--<div class="column has-text-centered">-->
-                <!--<div >-->
-                    <!--<span class="sliderScale editableText" id="scaleLeft"><strong>{{this.labels["left scale label"]}}</strong></span>-->
-                    <!--<span class="sliderScale editableText" id="scaleRight"><strong>{{this.labels["right scale label"]}}</strong></span>-->
-                    <!--<div >-->
-                        <!--<br>-->
-                        <!--<SliderRange> </SliderRange>-->
-                        <!--<span >{{this.labels["top slider’s label"]}}</span>-->
-                    <!--</div>-->
-                <!--</div>-->
-            <!--</div>-->
-
-        <!--</div>-->
 
   </div>
 </template>
 
 <script>
+    import SliderRangeTest from './components/SliderRange.vue';
     import textEn from "./assets/json/textEn.json";
     import textFr from "./assets/json/textFr.json";
-    import slider from "./assets/json/config.json";
-    import SliderRange from './components/SliderRange.vue';
+    import config from "./assets/json/config.json";
 
     export default {
         name: 'app',
         components: {SliderRange},
-            data() {
-              return {
-                  isLangauageChanged: true,
-                  sliderRanger: [],
+        data() {
+          return {
+              isLangauageChanged: true,
+              sliderRanges: [],
+              numberOfSlider: 0
+          };
+        },
+        props: {
 
-              };
+        },
+        methods: {
+            // METHOD DESCRIPTION
+            changeLanguage() {
+                if (this.isLanguageChanged)
+                    this.labels = textFr;
+                else
+                    this.labels = textEn;
+                this.isLanguageChanged = !this.isLanguageChanged;
+                this.$forceUpdate();
             },
-            props: {},
-            methods: {
-                // METHOD DESCRIPTION
-                changeLanguage() {
-                    if (this.isLanguageChanged)
-                        this.labels = textFr;
-                    else
-                        this.labels = textEn;
-                    this.isLanguageChanged = !this.isLanguageChanged;
-                    this.$forceUpdate();
-                },
-                managerSliderValue(){
-                   let val = this.$refs.slider.getValue();
-                   let setVal =this.$refs.slider.getIndex();
-                    if(val >setVal){
-                        this.$refs.slider.index[1].value++;
 
-                    }
-                }
-            },
-            created(){
-                this.labels = textEn;
+            // METHOD DESCRIPTION
+            // managerSliderValue(){
+            //    let val = this.$refs.slider.getValue();
+            //    let setVal =this.$refs.slider.getIndex();
+            //     if(val >setVal){
+            //         this.$refs.slider.index[1].value++;
+            //
+            //     }
+            // }
+        },
+        created(){
+            this.labels = textEn;
 
-            },
-        // mounted(){
-        //     let val = this.$refs.slider.getValue();
-        //     console.log("value" , val);
-        //     // let setVal =this.$refs.slider.getIndex();
-        //     // if(val >50){
-        //     //     this.$refs.slider.index[1].value++;
-        //     //     console.log("value" , val);
-        //     //
-        //     // }
-        // }
+        },
+        mounted(){
+            this.numberOfSlider = config.numberOfSlider;
+        }
     }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: left;
-  /*margin-top: 60px;*/
-}
-/*p {*/
-  /*display: flex;*/
-  /*justify-content: center;*/
-  /*text-align: left;*/
-  /*!*text-align: center;*!*/
-  /*!*float: center;*!*/
-/*}*/
-/**, *:before, *:after {*/
-  /*-moz-box-sizing: border-box; -webkit-box-sizing: border-box; box-sizing: border-box;*/
-/*}*/
+    #app {
+      font-family: 'Avenir', Helvetica, Arial, sans-serif;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+      text-align: left;
+    }
 
-h1 {
-  color:#70ada6;
-  font-size:1.4em;
-}
-#scaleLeft {
-  float: left;
-  margin-left: 5px;
-  text-align: left;
-}
-#scaleRight {
-  float: right;
-  margin-right: 5px;
-  text-align: right;
-}
-#slidersScale {
-  overflow:auto;
-}
+    h1 {
+      color:#70ada6;
+      font-size:1.4em;
+    }
+    #scaleLeft {
+      float: left;
+      margin-left: 5px;
+      text-align: left;
+    }
+    #scaleRight {
+      float: right;
+      margin-right: 5px;
+      text-align: right;
+    }
+    #slidersScale {
+      overflow:auto;
+    }
 </style>
---
+++ src/components/SliderRange_test.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="flex">
+        <vue-slider ref="slider"
+                    v-model="value"
+                    :processStyle="option.processStyle"
+                    :height="option.lineHeight"
+                    :dotSize="30"
+                    :min="0"
+                    :max="100"
+                    :disabled="false"
+                    :interval="1"
+                    :tooltip="'none'"
+                    :contained="true"
+                    @change="this.handlechange"
+                    :duration="inDragging ? 0 : 0.5"
+                    @drag-start="() => inDragging = true"
+                    @drag-end="() => inDragging = false"
+        ></vue-slider>
+    </div>
+</template>
+
+<script>
+    // import config from "../assets/json/config.json";
+    import VueSlider from 'vue-slider-component'
+    import 'vue-slider-component/theme/antd.css'
+    export default {
+        components: {VueSlider},
+        name: "SliderRangeTest",
+        data () {
+            return {
+                inDragging: false,
+                initialValue: 50,
+                option:{
+                    inDragging: false,
+                    lineHeight: 20,
+                    processStyle: {
+                        backgroundColor: 'blue'
+                    }
+                },
+                value: 50
+            }
+        },
+        methods: {
+            handlechange(){
+                // i have to use a for loop here to handle the change properly.
+                //i can see change on th slider
+                let value = this.$refs.slider.getValue();
+                console.log("value" , value);
+            }
+
+        },
+        mounted () {},
+    }
+</script>
+
+<style scoped>
+
+</style>
