914d5a50837efc98b22528bdf48867c55645f62d
naddyn <nadege.touko-toudjeu.1@ulaval.ca>
2019-05-29 01:25:32 -0400

    Some test to get value of one slider and set the value of th second slider and vice-versa, Nt complete

--- src/App.vue
+++ src/App.vue
@@ -10,29 +10,29 @@
             <br/>
         </div>
         <div class="column is-half has-text-centered">
-            <span id="scaleLeft">{{this.labels["left scale label"]}}</span>
-            <span id="scaleRight">{{this.labels["right scale label"]}}</span>
+            <span id="scaleLeft">{{this.labels.leftScaleLabel}}</span>
+            <span id="scaleRight">{{this.labels.rightScaleLabel}}</span>
             <br>
             <!-- <SliderRangeTest></SliderRangeTest> -->
             <!-- <div ref="sliders" v-for="(slider) in this.numberOfSlider" :key="SliderRangeTest.id"> -->
-            <div v-for="(slider) in this.numberOfSlider">
-                <SliderRange></SliderRange>
+            <div  v-for="(slider) in this.numberOfSlider" ref="its"  :key="slider">
+                <SliderRange v-bind:ref="slider"></SliderRange>
             </div>
-            <span >{{this.labels["top slider’s label"]}}</span>
+            <span >{{this.labels.topSliderLabel}}</span>
+
+            <button @click="getValueOfId(1)">handle</button>
+            <button @click="set">set</button>
         </div>
 
   </div>
 </template>
 
 <script>
-    import SliderRangeTest from './components/SliderRange.vue';
+    import SliderRange from './components/SliderRange.vue';
     import textEn from "./assets/json/textEn.json";
     import textFr from "./assets/json/textFr.json";
-<<<<<<< HEAD
-
-=======
     import config from "./assets/json/config.json";
->>>>>>> b953129a74a58c4d94c0c0998ca80819417305e0
+
 
     export default {
         name: 'app',
@@ -57,6 +57,67 @@
                 this.isLanguageChanged = !this.isLanguageChanged;
                 this.$forceUpdate();
             },
+            getValueOfId(sliderId){
+
+                let valueSlider1 = this.$refs[sliderId][0].handlechange();
+                let valueSlider2 = this.$refs[sliderId +1][0].handlechange();
+
+                console.log("la valeur du slider 1", valueSlider1);
+                console.log("la valeur du slider 2", valueSlider2);
+                // // valueSlider1 == valueSlider2;
+                // //
+                // this.refs[sliderId][0].setValue(valueSlider1);
+
+                // for(let i = 1; i<= this.$refs[sliderId][0].length;  i++){
+                //         if(valueSlider1 > 50){
+                //
+                //         }
+                //
+                // }
+
+
+            },
+            /***
+             * methode to change the background cplor for each slider in the div
+             * started
+             */
+            set() {
+                console.log("helo");
+                // this.$refs["its"][1].style.border = "1px solid red";
+                var src = this.$refs[1][0].handlechange();
+                var target = this.$refs[2][0].handlechange();
+
+                //gettting index to see changes
+                // let index1 = this.$refs[1][0].getSliderIndex();
+                // let index2 = this.$refs[2][0].getSliderIndex();
+                // let  currentIndex = index1 - 49;
+                //
+                // console.log("current" ,currentIndex);
+                //
+                // console.log("index 1", index1);
+
+                // this.$refs[2][0].setSlider(20);  //setting value of slider
+                if(src ++ || src--){
+                    // target -= 100 -src;
+
+
+                    this.$refs[2][0].setSlider(100 - src);
+                    console.log("new src" , src);
+                    console.log("new target " , target);
+                } else if (target++ || target--) {
+
+                    this.$refs[1][0].setSlider(101 - target);
+                }
+                // console.log("src" ,src);
+                // console.log("target",target);
+
+                for(let i=0; i<=this.$refs["its"].length; i++ ){
+                    this.$refs["its"][i].style.backgroundColor = config.computedColor2;
+                }
+
+
+            },
+
 
             // METHOD DESCRIPTION
             // managerSliderValue(){
@@ -71,9 +132,16 @@
         created(){
             this.labels = textEn;
 
+
+
         },
-        mounted(){
+        mounted() {
             this.numberOfSlider = config.numberOfSlider;
+            // this.$refs.its[0].style.border = "1px solid red";
+             //var value = this.$refs.its[1].handlechange();
+            // var value2 = this.$refs[2][0].handlechange();
+            // console.log("value1", value);
+            // console.log("value2", value2);
         }
     }
 </script>
--- src/components/SliderRange.vue
+++ src/components/SliderRange.vue
@@ -1,21 +1,17 @@
 <template>
     <div >
-        <div>
-            <div v-bind:style="styleObject1">
-                <vue-slider ref="slider1" v-model="value1" v-bind="option" :processStyle="option.processStyle2" @change="this.handlechange" :contained="true" :duration="inDragging ? 0 : 0.5" @drag-start="() => inDragging = true"
-                            @drag-end="() => inDragging = false"
+        <div class="flex">
+            <vue-slider ref="slider"
+                        v-model="value"
+                        v-bind="option"
+                        :processStyle="option.processStyle"
+                        @change="this.handlechange "
+                        :contained="true"
+                        :duration="inDragging ? 0 : 0.5"
+                        @drag-start="() => inDragging = true"
+                        @drag-end="() => inDragging = false"
 
-                ></vue-slider>
-
-            </div>
-            <div v-bind:style="styleObject2">
-                <vue-slider ref="slider2" v-model="value2" v-bind="option" :processStyle="option.processStyle1" @change="this.handlechange" :contained="true"  :duration="inDragging ? 0 : 0.5" @drag-start="() => inDragging = true"
-                            @drag-end="() => inDragging = false"
-
-                ></vue-slider>
-            </div>
-
-
+            ></vue-slider>
 
 
             <!--some test to get  value of slider-->
@@ -25,8 +21,6 @@
             <!--<button @click="$refs.slider.setIndex(50)">setIndex</button>-->
             <!--<button @click="logIndex($refs.slider.getIndex())">getIndex</button>-->
             <!--</div>-->
-
-
         </div>
     </div>
 
@@ -42,113 +36,66 @@
         data () {
             return {
                 inDragging: false,
-
                 slider: [],
-
-                styleObject1:{
-                  backgroundColor: config.computedColor1,
-                },
-                styleObject2:{
-                    backgroundColor: config.computedColor2,
-                },
-
-                value1: config.value1,
-                value2: config.value2,
-
+                // styleObject1:{
+                //   backgroundColor: config.computedColor1,
+                // },
+                initialValue: config.value,
                 option:{
                     inDragging: false,
                     height: config.lineHeight,
-                    processStyle2: {
-                        backgroundColor:config.backgroundColor2,
+                    processStyle: {
+                        backgroundColor:config.backgroundColor,
                   },
-                    processStyle1: {
-                        backgroundColor:config.backgroundColor1,
-                  },
-                    // processStyle:config.backgroundColor2,
+
                     dotSize:config.dotSize,
                     min: config.min,
                     max: config.max,
                     interval: config.interval,
                     tooltip: 'none',
 
-
-
-
                 },
+                value: 50,
             }
         },
 
         methods: {
-            // logValue (v) {
-            //     window.alert("Value: "+ v)
-            // },
-            // logIndex (v) {
-            //     window.alert("Index: "+ v)
-            // }
             handlechange() {
                 // i have to use a for loop here to handle the change properly.
                 //i can see change on th slider
-                let val1 = this.$refs.slider1.getValue();
-                let val2 = this.$refs.slider2.getValue();
-                let index1 = this.$refs.slider1.getIndex();
-                let index2 = this.$refs.slider2.getIndex();
-                if (val1 >this.value1) {
-                    console.log("index1", index1);
-                }
+                // let value = this.$refs.slider.getValue();
+                // console.log("value" , value);
+                let value = this.$refs.slider.getValue();
+                return value;
+
+
+            },
+            setSlider(value){
+                this.$refs.slider.setValue(value);
+            },
+            getSliderIndex(){
+                let index = this.$refs.slider.getIndex();
+                return index;
+
             }
 
-                // for(let i = 1; i<=this.$refs.slider1.max(); i++){
-                //     console.log("i value", i);
-                //     let val1 = this.$refs.slider1.getValue();
-                //     let val2 = this.$refs.slider2.getValue();
-                //
-                //     if(val1 > 50){
-                //         this.value2 -= i;
-                //         // console.log("i value", i);
-                //     }
-                // }
-            // }
-            //     // console.log("value1" , val1);
-            //     // console.log("value2" , val2);
-            //     if(val1 > 50){
-            //        // let  indexVal1 = this.$refs.slider1.getValue();
-            //        // console.log("index" , indexVal1)
-            //         this.value2 --;
-            //         // console.log("value" , this.value2);
-            //     }else if(val1 < 50){
-            //         this.value2 ++;
-            //     }else if(val2 > 50){
-            //         this.value1--;
-            //     }else if(val2<50){
-            //         this.value1 ++;
-            //     }
-            // }
+
 
         },
         mounted () {
             document.body.addEventListener('keydown', (e) => {
-                let index1 = this.$refs.slider1.getValue();
-                let index2 = this.$refs.slider2.getValue();
                 // console.log("test", this.value1);
-                if (e.keyCode === 39 &&  index1 <= config.max && index2 >= config.min) {
-                    this.value1++ && this.value2--;
-
-                    console.log("value1",this.value1);
+                if (e.keyCode === 39) {
+                    this.value++
                 }
-                if (e.keyCode === 37 && index2 <= config.max && index1 >= config.min) {
+                if (e.keyCode === 37) {
 
-                    this.value1-- && this.value2++;
+                    this.value--;
                 }
             })
-            // let val1 = this.$refs.slider1.getValue();
-            // let val2 = this.$refs.slider1.getValue();
-            // console.log("value1" , val1);
-            // console.log("value2" , val2);
-            // if(val1>= 50){
-            //     this.value2 --;
-            //     console.log("value" , this.value2);
-            // }
+
         },
+
 
 
 
