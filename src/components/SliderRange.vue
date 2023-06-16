8070c10ee1b61b25d053fd46c1bc44ef28abe570
Subatomail <yazini.ismail@gmail.com>
2023-06-15 21:27:31 -0400

    responsive done

--- src/components/SliderRange.vue
+++ src/components/SliderRange.vue
@@ -1,48 +1,99 @@
 <template>
-  <div class="d-flex flex-column">
-    <div>
-      <div class="text-caption">Show thumb when using slider</div>
-      <v-slider v-model="slider1" thumb-label></v-slider>
-    </div>
+  <v-container fluid>
+    <v-row no-gutters>
+      <v-col order-sm="first">
+        <div class="d-flex flex-column">
+          <div class="d-flex justify-space-between">
+            <div class="font-weight-regular">Show thumb</div>
+            <div class="font-weight-regular">Show thumb</div>
+          </div>
+          <div class="colored-box1 mb-2">
+            <div class="d-flex flex-column pa-4">
+              <v-slider
+                v-model="slider1"
+                thumb-label
+                color="#410e73"
+                thumb-color="white"
+                thumb-size="40"
+                track-size="30"
+                track-color="white"
+                rounded="xl"
+              ></v-slider>
+              <div class="font-weight-regular text-center">
+                Show thumb when using slider
+              </div>
+            </div>
+          </div>
 
-    <div>
-      <div class="text-caption">Show thumb when using slider</div>
-      <v-slider v-model="slider2" thumb-label></v-slider>
-    </div>
-    <vertical-progress-bar
-      :choice1="choice1"
-      :choice2="choice2"
-    ></vertical-progress-bar>
-  </div>
+          <div class="colored-box2">
+            <div class="d-flex flex-column pa-4">
+              <v-slider
+                v-model="slider2"
+                thumb-label
+                color="#a8a873"
+                thumb-color="white"
+                thumb-size="40"
+                track-size="30"
+                track-color="white"
+                rounded="xl"
+              ></v-slider>
+              <div class="font-weight-regular text-center">
+                Show thumb when using slider
+              </div>
+            </div>
+          </div>
+        </div>
+      </v-col>
+      <v-col order="first">
+        <div>
+          <vertical-progress-bar
+            :value1="slider1"
+            :value2="slider2"
+          ></vertical-progress-bar>
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 import VerticalProgressBar from "./VerticalProgressBar.vue";
+import { ref, watch } from "vue";
+
 export default {
   components: {
     VerticalProgressBar,
   },
-  data() {
+  setup() {
+    const slider1 = ref(50);
+    const slider2 = ref(50);
+
+    const choice1 = ref(slider1.value);
+    const choice2 = ref(slider2.value);
+
+    watch(slider1, (newValue) => {
+      slider2.value = 100 - newValue;
+    });
+
+    watch(slider2, (newValue) => {
+      slider1.value = 100 - newValue;
+    });
+
     return {
-      slider1: 50,
-      slider2: 50,
+      slider1,
+      slider2,
+      choice1,
+      choice2,
     };
-  },
-  computed: {
-    choice1() {
-      return this.slider1;
-    },
-    choice2() {
-      return this.slider2;
-    },
-  },
-  watch: {
-    slider1(newValue) {
-      this.slider2 = 100 - newValue;
-    },
-    slider2(newValue) {
-      this.slider1 = 100 - newValue;
-    },
   },
 };
 </script>
+
+<style scoped lang="scss">
+.colored-box1 {
+  background-color: #e6e5ff;
+}
+.colored-box2 {
+  background-color: #f1f4b6;
+}
+</style>
--- src/components/VerticalProgressBar.vue
+++ src/components/VerticalProgressBar.vue
@@ -1,41 +1,82 @@
 <template>
-  <v-container style="width: 300px;" fluid class="vertical-progress-container">
-    <v-progress-linear
-      v-model="power"
-      color="amber"
-      height="80"
-      class="progressBar"
-    ></v-progress-linear>
-    <div class="space_between_div"></div>
-    <v-progress-linear
-      v-model="skill"
-      color="blue-grey"
-      height="80"
-      class="progressBar"
-    >
-    </v-progress-linear>
+  <v-container class="d-flex flex-column align-center">
+    <v-container style="width: 250px" fluid class="vertical-progress-container">
+      <div class="d-flex flex-column">
+        <div class="mb-12">
+          <v-progress-linear
+            v-model="value1"
+            color="#410e73"
+            height="80"
+            class="progressBar"
+          ></v-progress-linear>
+        </div>
+        <v-spacer class="mb-12"></v-spacer>
+        <div>
+          <v-progress-linear
+            v-model="value2"
+            color="#a8a873"
+            height="80"
+            class="progressBar"
+          >
+          </v-progress-linear>
+        </div>
+      </div>
+    </v-container>
+    <div style="width: 288px" class="d-flex justify-space-between mb-6 px-6">
+      <div class="font-weight-regular text-center">
+        <span style="color: #410e73">Option 1</span>
+      </div>
+      <div class="font-weight-regular text-center">
+        <span style="color: #a8a873">Option 2</span>
+      </div>
+    </div>
+    <div class="d-flex justify-center" v-if="value1 !== 50">
+      <v-chip variant="outlined" :color="value1 > 50 ? '#410e73' : '#a8a873'">
+        <b>Option 2&nbsp;</b>fits best with what matters to you
+      </v-chip>
+    </div>
   </v-container>
 </template>
   
-  <script>
+<script>
+import { ref, watch } from "vue";
+
 export default {
-  data: () => ({
-    skill: 50,
-    power: 78,
-  }),
+  props: {
+    value1: {
+      type: Number,
+      required: true,
+    },
+    value2: {
+      type: Number,
+      required: true,
+    },
+  },
+  setup(props) {
+    const value1 = ref(props.value1);
+    const value2 = ref(props.value2);
+    // Watch the prop values for changes and update the refs accordingly
+    watch(
+      () => props.value1,
+      (newValue) => {
+        value1.value = newValue;
+      }
+    );
+
+    watch(
+      () => props.value2,
+      (newValue) => {
+        value2.value = newValue;
+      }
+    );
+    return { value1, value2 };
+  },
 };
 </script>
 
 <style scoped lang="scss">
 .vertical-progress-container {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
   transform: rotate(-90deg);
 }
-.space_between_div {
-        margin: auto;
-        padding: 20% 0;
-    }
 </style>
 
